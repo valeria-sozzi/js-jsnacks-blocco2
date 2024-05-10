6fa010dc1dc11971f9927c49c7144fa680004205
Valeria Sozzi <valeriasozzi24@gmail.com>
2024-05-10 17:15:41 +0200

    add snack-5 completed

--- snack-5/main.js
+++ snack-5/main.js
@@ -1,1 +1,16 @@
 //crea due array che hanno un numero di elementi diversi. aggiungi gli elementi casuali all'array che ha meno elementi fino a quando ne avrà tanti qunati l'altro
+
+//crea due array di diversa lunghezza
+const deskList = ['pc', 'agenda', 'ipad', 'borraccia', 'penne', 'candela', 'mouse', 'caricatori']
+const roomList = ['divano', 'tv', 'play', 'scrivania']
+
+//aggiungere elementi causali all'array più corta finche le due array non hanno la stessa lunghezza
+
+let i=4;
+
+
+while (roomList.length !== deskList.length){
+    roomList.push(deskList[i])
+    console.log(roomList);
+    i++
+}
